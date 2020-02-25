575fe2596f5bc6aa6bf33fd245b29b0b45de6d63
Max <max-dau@users.noreply.github.com>
2020-02-25 08:50:06 +0100

    .

--- nimGame/nimGame.py
+++ nimGame/nimGame.py
@@ -1,4 +1,4 @@
-rows = [1, 3, 5, 7]
+rows = [1, 2, 5, 7]
 groups = [
     [],
     [],
@@ -10,46 +10,81 @@ groups = [
 #Continues for as long as it takes the user to get the input right.
 def userChoice():
     while True:
-        global row
-        row = int(input('Out of which row would you like to take a match? (input number)\n'))
-        if row > 4 or row <= 0:
+        global rowChoice
+        rowChoice = int(input('Out of which row would you like to take a match? (input number)\n'))
+        if rowChoice > 4 or rowChoice <= 0:
             print('There only are 4 rows. Choose another one.')
-        elif rows[row-1] == 0:
+        elif rows[rowChoice-1] == 0:
             print('There\'s nothing in this row. Choose another one.')
         else:
             break
     while True:
         global amount
         amount = int(input('How many matches would you like to take out of this row?\n'))
-        if amount > rows[row-1]:
+        if amount > rows[rowChoice-1]:
             print('This is a higher number than there are matches in this row. Choose another amount\n')
         elif amount == 0:
             print('You can\'t take zero matches out of a row.')
         else:
             break
 
-#computerChoice goups the rows into groups of 4, 2 and 1 matches and chooses on which matches from which row to get rid of
+#computerChoice first checks, if the computer is within striking distance of winning, if yes does accordingly
+#if not groups the rows into groups of 4, 2 and 1 matches and chooses on which matches from which row to get rid of
 #in order to "balance" the board. a balanced board is a board, which always has
 def computerChoice():
-    index = 0
+    filledRows = []
     for i in rows:
-        matchesLeft = i
-        while matchesLeft > 0:
-            if matchesLeft >= 4:
-                groups[index].append(4)
-                matchesLeft -= 4
-            elif matchesLeft >= 2:
-                groups[index].append(2)
-                matchesLeft -= 2
-            elif matchesLeft == 1:
-                groups[index].append(1)
-                matchesLeft -= 1
-        index += 1
-        print(groups)
+        if rows[i] > 0:
+            filledRows.append(rows[i])
+    if filledRows <= 2 and :
+        #figure out what to do
+    else:
+        index = 0
+        for i in rows:
+            matchesLeft = i
+            while matchesLeft > 0:
+                if matchesLeft >= 4:
+                    groups[index].append(4)
+                    matchesLeft -= 4
+                elif matchesLeft >= 2:
+                    groups[index].append(2)
+                    matchesLeft -= 2
+                elif matchesLeft == 1:
+                    groups[index].append(1)
+                    matchesLeft -= 1
+            index += 1
+
+#checks the balance of the board and chooses amount and row based on it
+        if groups.count(4) % 2 not == 0:
+            for i in rows:
+                if rows[i] >= 4:
+                    i = rowChoice
+                    amount = 4
+                i += 1
+        elif groups.count(2) % 2 not == 0:
+            for i in rows:
+                if rows[i] >= 2:
+                    i = rowChoice
+                    amount = 2
+                i += 1
+        elif groups.count(1) % 2 not == 0:
+            for i in rows:
+                if rows[i] >= 1:
+                    i = rowChoice
+                    amount = 1
+                i += 1
+
 
 def updateRows():
-    rows[row-1] -= amount
+    rows[rowChoice-1] -= amount
 
 #prints the entire board.
 def printboard():
-    print('')
+    print('This is the board right now.')
+    row = 0
+    while row <= 3:
+        outputLine = ''
+        for i in range(0, rows[row]):
+            outputLine += 'I '
+        print(outputLine)
+        row += 1
