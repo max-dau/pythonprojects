803d12a1d3c41eaed2a273b299f33d04511d03b2
Max <36899255+max-dau@users.noreply.github.com>
2020-02-28 16:37:33 +0100

    finished up tictactoe

--- tictactoe/tictactoe.py
+++ tictactoe/tictactoe.py
@@ -1,27 +1,73 @@
-print('Hey! Welcome to Tic-Tac-Toe! This game is played by two players.\nPlayer 1 is represented by X and Player 2 by O.')
-print('  1  2  3 \n  -  -  - \nA|  |  |  |\n  -  -  - \nB|  |  |  |\n  -  -  - \nC|  |  |  |\n  -  -  - ')
-print('This is the and it\'s coordinates. The letter is the first coordinate and the number is the second coordinate.\nSo if I want to place my symbol in the upper right corner, I type \'A1\'.')
+from time import sleep
 
-viableX = ['A', 'B', 'C']
-viableY = ['1', '2', '3']
+#get player input as a coordinate and check whether it will work with the program
+viableCoords = ['1', '2', '3']
 
-#function to get player input as a coordinate and check whether it will work with the program
-def getInputAndCheck():
-    print('Where do you want to place your symbol?')
+def getInput(player):
     while True:
-        coord = input('Where do you want to place your X?\n')
-
-        if coord[0] not in viableX or coord[1] not in viableY:
+        coords = input('Where do you want to place your symbol?\n')
+        if coords[0] not in viableCoords or coords[1] not in viableCoords:
             print('Your input seems to be illogical. Please try again.')
             continue
+        elif grid[int(coords[0])-1][int(coords[1])-1] != ' ':
+            print('There already is a symbol in this spot. please choose another one.')
         else:
+            grid[int(coords[0])-1][int(coords[1])-1] = player
             break
 
+
 #All lines get filled with "-" for empty at the beginning
-A = ['-', '-', '-']
-B = ['-', '-', '-']
-C = ['-', '-', '-']
+grid = [
+    [' ', ' ', ' '],
+    [' ', ' ', ' '],
+    [' ', ' ', ' ']
+]
 
 #Checks the board for a win, takes every symbol and tests for a line going horizontal, vertical, or across
 def checkForWin():
-    while True:
+    row = 0
+    while row < 3:
+        if grid[row][0] == grid[row][1] == grid[row][2] != ' ':
+            return 'win'
+        row += 1
+    column = 0
+    while column <3:
+        if grid[0][column] == grid[1][column] == grid[2][column] != ' ':
+            return 'win'
+        column += 1
+    else:
+        if grid[0][0] == grid[1][1] == grid[2][2] != '-' or grid[0][2] == grid[1][1] == grid[2][0] != ' ':
+            return 'win'
+        elif ' ' not in grid[0] and ' ' not in grid[1] and ' ' not in grid[2]:
+            return 'tie'
+        else:
+            return False
+
+def printBoard():
+    print('  1  2  3 \n  -  -  - \n1| '+grid[0][0]+'| '+grid[0][1]+'| '+grid[0][2]+'|\n  -  -  - \n2| '+grid[1][0]+'| '+grid[1][1]+'| '+grid[1][2]+'|\n  -  -  - \n3| '+grid[2][0]+'| '+grid[2][1]+'| '+grid[2][2]+'|\n  -  -  - ')
+
+print('Hey! Welcome to Tic-Tac-Toe! This game is played by two players.\nPlayer 1 is represented by X and Player 2 by O. Player 1 starts.')
+printBoard()
+print('This is the grid and it\'s coordinates. The first letter is the row and the second letter the column.\nSo if I want to place your symbol in the upper row in the middle, type \'12\'.')
+
+while True:
+    getInput('X')
+    sleep(0.3)
+    printBoard()
+    sleep(0.5)
+    if checkForWin() == 'win':
+        print('Player 1 (X) has won!')
+        break
+    elif checkForWin() == 'tie':
+        print('You tied!')
+        break
+    getInput('O')
+    sleep(0.3)
+    printBoard()
+    if checkForWin() == 'win':
+        print('Player 1 (O) has won!')
+        break
+    elif checkForWin() == 'tie':
+        print('You tied!')
+        break
+    sleep(0.5)
